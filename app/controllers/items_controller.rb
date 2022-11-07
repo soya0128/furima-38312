6f63e872ae040240a6bf107e4075d8b036c1ef16
soya0128 <bori.rhythem2545@gmail.com>
2022-11-07 22:45:11 +0900

    ルーティング、コントローラー、モデルの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,16 @@ class ItemsController < ApplicationController
   def index
   end
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, :user_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :image,              null: false
+  validates :name,               null: false
+  validates :text,               null: false
+  validates :category_id,        null: false
+  validates :condition_id,       null: false
+  validates :delivery_charge_id, null: false
+  validates :prefecture_id,      null: false
+  validates :delivery_day_id,    null: false
+  validates :price,              null: false
+  validates :user_id,            null: false, foreign_key: true
 end
