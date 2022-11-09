19f5fbe3d5fa84dd0a7f8df3e5167cd678b5272c
soya0128 <bori.rhythem2545@gmail.com>
2022-11-09 22:00:50 +0900

    モデルファイルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,14 @@ class Item < ApplicationRecord
   validates :name,               presence: true
   validates :text,               presence: true
   validates :category_id,        presence: true, numericality: {other_than: 1}
-  validates :condition_id,       presence: true
-  validates :delivery_charge_id, presence: true
-  validates :prefecture_id,      presence: true
-  validates :delivery_day_id,    presence: true
+  validates :condition_id,       presence: true, numericality: {other_than: 1}
+  validates :delivery_charge_id, presence: true, numericality: {other_than: 1}
+  validates :prefecture_id,      presence: true, numericality: {other_than: 1}
+  validates :delivery_day_id,    presence: true, numericality: {other_than: 1}
   validates :price,              presence: true
   validates :user_id,            presence: true
 
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
     belongs_to :condition
