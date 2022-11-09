2deeb899086f519d27e3ae52c4329f0d112bfebe
soya0128 <bori.rhythem2545@gmail.com>
2022-11-09 20:45:04 +0900

    categoryモデルの作成、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,14 @@ class Item < ApplicationRecord
   validates :image,              presence: true
   validates :name,               presence: true
   validates :text,               presence: true
-  validates :category_id,        presence: true
+  validates :category_id,        presence: true, numericality: {other_than: 1}
   validates :condition_id,       presence: true
   validates :delivery_charge_id, presence: true
   validates :prefecture_id,      presence: true
   validates :delivery_day_id,    presence: true
   validates :price,              presence: true
   validates :user_id,            presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
