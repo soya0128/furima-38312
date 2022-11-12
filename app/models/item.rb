38341a935967bd2b67977bf2e0f7cef75d33aa17
soya0128 <bori.rhythem2545@gmail.com>
2022-11-12 18:03:33 +0900

    itemモデルファイル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,12 @@
 class Item < ApplicationRecord
-  validates :image,              presence: true
-  validates :name,               presence: true
-  validates :text,               presence: true
+  validates :name,               presence: true, length:{ maximum: 40 }
+  validates :text,               presence: true, length:{ maximum: 1000 }
   validates :category_id,        presence: true, numericality: {other_than: 1}
   validates :condition_id,       presence: true, numericality: {other_than: 1}
   validates :delivery_charge_id, presence: true, numericality: {other_than: 1}
   validates :prefecture_id,      presence: true, numericality: {other_than: 1}
   validates :delivery_day_id,    presence: true, numericality: {other_than: 1}
   validates :price,              presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  validates :user_id,            presence: true
-
   
   has_one_attached :image
   belongs_to :user
