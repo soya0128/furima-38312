e6acea69adaae98de7f5606d77425d8717029c72
soya0128 <bori.rhythem2545@gmail.com>
2022-11-13 20:34:13 +0900

    itemモデルの修正（imageバリデーションを追加）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  validates :image,              presence: true
   validates :name,               presence: true, length:{ maximum: 40 }
   validates :text,               presence: true, length:{ maximum: 1000 }
   validates :category_id,        presence: true, numericality: {other_than: 1}
