add60d41e3b20462fdf80ced69928f22ce279a8c
soya0128 <bori.rhythem2545@gmail.com>
2022-11-13 20:33:53 +0900

    テストコードのためのFactoryBot用ファイルの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Alphanumeric.alphanumeric(number: 6)}
+    password              {Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1,min_numeric: 1)}
     password_confirmation {password}
     last_name             {'鈴木'}
     first_name            {'太郎'}
