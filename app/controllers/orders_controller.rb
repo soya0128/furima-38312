c7cac21ea8cf5eb296b81198c704ae4e928b32b6
soya0128 <bori.rhythem2545@gmail.com>
2022-11-26 19:45:14 +0900

    ordersコントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,38 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_params, only:[:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
-    @order_address = OrderAddress.new
+    if @item.order != nil
+      redirect_to root_path
+    elsif current_user.id != @item.user.id
+      @order_address = OrderAddress.new
+    end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: order_params[:price],
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @order_address.save
-      redirect_to root_path
+      return redirect_to root_path
     else
-      render :index
+      render 'index'
     end
   end
 
   private
+  def set_params
+    @item = Item.find(params[:item_id])
+  end
+
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], price: @item.price, token: params[:token])
   end
 
 end
