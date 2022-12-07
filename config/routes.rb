454e56b4e8e24b3ead28a69ab4f9b4875ba00843
soya0128 <bori.rhythem2545@gmail.com>
2022-12-07 19:10:47 +0900

    ユーザーマイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   devise_for :users
   root to: 'items#index'
+  resources :users, only: [:show, :update]
   resources :items do
     resources :orders, only:[:index, :create]
   end
+  resources :cards, only: [:new, :create]
 end
