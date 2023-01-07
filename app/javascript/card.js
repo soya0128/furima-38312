28aed88ae8b5c18041cdf2eab9b9b60609d4a312
soya0128 <bori.rhythem2545@gmail.com>
2023-01-07 22:18:44 +0900

    card.jsの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,26 +1,29 @@
 const order = () => {
   const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber')
-  const cvcElement = elements.create('cardCvc')
-  const expiryElement = elements.create('cardExpiry')
-  numberElement.mount('#number')
-  cvcElement.mount('#cvc')
-  expiryElement.mount('#exp-date')
 
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
+  const numberElement = elements.create('cardNumber')
+  const expiryElement = elements.create('cardExpiry')
+  const cvcElement = elements.create('cardCvc')
+
+  numberElement.mount('#number-form')
+  expiryElement.mount('#expiry-form')
+  cvcElement.mount('#cvc-form')
+
+  const submit = document.getElementById("button");
+  button.addEventListener("click", (e) => {
     e.preventDefault();
-    payjp.createToken(expiryElement).then((response) => {
+    payjp.createToken(numberElement).then(function(response) {
       if (response.error){
+      } else {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form")
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      const token = response.id;
-      const renderDom = document.getElementById("charge-form")
-      const tokenObj = `<input value=${token} name='token' type="hidden">`;
-      renderDom.insertAdjacentHTML("beforeend", tokenObj);
       document.getElementById("charge-form").submit();
     });
-  });
-};
+  })
+}
 
 window.addEventListener('load', order);
